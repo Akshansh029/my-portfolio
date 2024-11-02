c254a8bab776292125eb21ac3aaf53c2810879d7
Akshansh <akshanshsingh00@gmail.com>
2024-11-02 19:58:13 +0530

    View projects button added

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,8 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
@@ -19,6 +21,14 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
+      <Link
+        className="group bg-gray-900 text-white text-sm px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-105 transition mt-4 w-fit"
+        href="https://github.com/Akshansh029?tab=repositories"
+        target="_blank"
+      >
+        View all projects on GitHub{" "}
+        <BsArrowRight className="opacity-90 group-hover:translate-x-1 transition" />
+      </Link>
     </section>
   );
 }
