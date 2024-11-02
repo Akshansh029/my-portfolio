5bbf8208042cbbe6eae3420553c2ab250ee2f844
Akshansh <akshanshsingh00@gmail.com>
2024-11-02 19:46:50 +0530

    Corrected about

--- components/about.tsx
+++ components/about.tsx
@@ -18,32 +18,6 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      {/* <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p> */}
       <p className="mb-3">
         {" "}
         I am a third-year{" "}
@@ -65,11 +39,9 @@ export default function About() {
         {" "}
         Outside of coding, I enjoy{" "}
         <span className="italic">playing football, enjoying video games</span>,
-        visiting fun spots in Chennai with friends, and exploring{" "}
-        <span className="font-medium">
-          new technologies and creative projects
-        </span>
-        . I’m seeking a <span className="font-medium">internship</span> to gain
+        learning something new and interesting, and exploring{" "}
+        <span className="font-medium">technologies and creative projects</span>.
+        I’m seeking a <span className="font-medium">internship</span> to gain
         industry experience and make meaningful contributions to a team.{" "}
       </p>
     </motion.section>
